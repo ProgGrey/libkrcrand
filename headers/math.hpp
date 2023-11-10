ded737b8f7f3a065291c221eb2f15314e3bcf182
Sergey Astaf'ev <seryymail@mail.ru>
2023-11-10 11:32:04 +0300

    Add lambertw0 function

--- headers/math.hpp
+++ headers/math.hpp
@@ -10,6 +10,7 @@ inline double type_depend_mull(double a, double b){
 }
 double uniform01_exclude0(uint64_t a);
 double unsafe_log(double x);
+double unsafe_lambertw0(double x);
 }
 #ifdef LIBKRCRAND_ENABLE_SSE2
 #include <emmintrin.h>
