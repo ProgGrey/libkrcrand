3296b0a7e97700baec03e0d93f0f884c4ce65aca
Sergey Astaf'ev <seryymail@mail.ru>
2024-04-19 01:42:22 +0300

    Added the cdflib - a c++ port of original fortran cdflib.

---
+++ src/3rdparty/cdflib.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+
+namespace n3rdPartyCDF{
+
+#include "./raw/cdflib.cpp"
+
+};
---
+++ src/3rdparty/cdflib.hpp
@@ -0,0 +1,18 @@
+# include <cmath>
+# include <cstdlib>
+# include <cstring>
+# include <ctime>
+# include <iomanip>
+# include <iostream>
+namespace n3rdPartyCDF{
+
+// Code below licensed under MIT license. 
+// See https://people.sc.fsu.edu/~jburkardt/cpp_src/cdflib/cdflib.html
+#include <string>
+using namespace std;
+#include "./raw/cdflib.hpp"
+
+#define GAMMA_CDF 1
+#define GAMMA_QF 2
+
+};
